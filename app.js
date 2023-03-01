bf5a2198f8d1f826cf0584d8df66b9ce2dd6f1ac
mgregerson <mgregerson4321@gmail.com>
2023-02-28 21:23:58 -0500

    added doc Strings for each function, added simple css to set stock height and width of each gif.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const $SUBMIT_BUTTON = $("#submit-button");
 const $DELETE_BUTTON = $("#delete-button");
 const $GIF_CONTAINER = $(".Gif-container");
 let $gifInput;
-// http://api.giphy.com/v1/gifs/search?q=
 
 /** Gets a random number between 0 and the number of gifs */
 
@@ -12,7 +11,12 @@ function getRandomInt(numOfGifs) {
   return Math.floor(Math.random() * numOfGifs);
 }
 
-/** */
+/** This callback function will fire on click of the submit button. It will find
+ * the value of our gifInput, and find a group of gifs associated with that input
+ * using the axios API. It will call our getRandomInt function to find one of the 50
+ * gifs in that list. Next, it will call addGifToContainer, using that random gif as
+ * our argument.
+ */
 
 async function getGif(event) {
   event.preventDefault();
@@ -21,15 +25,11 @@ async function getGif(event) {
   let groupOfGifs = await axios.get(
     `http://api.giphy.com/v1/gifs/search?q=${$gifInput}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
   );
-  // let randomGif = getRandomInt(groupOfGifs.data.data);
-  // console.log(groupOfGifs.data.data);
   let randomIndex = getRandomInt(groupOfGifs.data.data.length);
-  // console.log(groupOfGifs.data.data[randomIndex]);
-  console.log(groupOfGifs.data.data);
   addGifToContainer(groupOfGifs.data.data[randomIndex]);
 }
 
-/**  */
+/** addGifToContainer will append our new gif to the $GIF_CONTAINER div on our page.*/
 
 function addGifToContainer(gif) {
   let gifURL = gif.images.fixed_width.url;
@@ -37,19 +37,13 @@ function addGifToContainer(gif) {
   $GIF_CONTAINER.append($(`<img src=${gifURL}>`));
 }
 
+/** deleteGifs is a callback function that will fire when someone clicks the delete button.
+ * It will delete all gifs on our page.
+ */
+
 function deleteGifs() {
   $GIF_CONTAINER.content = "";
 }
 
 $SUBMIT_BUTTON.on("click", getGif);
 $DELETE_BUTTON.on("click", deleteGifs);
-
-// async function getCard() {
-//   let response = await axios.get(
-//     "/api/card");
-
-//   console.log("getCard resp=", response);
-//   $("#card").html(response.data);
-// }
-
-// $("#card-btn").on("click", getCard);
